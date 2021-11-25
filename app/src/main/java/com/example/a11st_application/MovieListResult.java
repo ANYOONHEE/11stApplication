d46128a86e5876608e6f13ab00f6a395cad3d699
안윤희 <an-yunhui@an-yunhuiui-iMac.local>
2021-11-25 10:27:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a11st_application/MovieListResult.java
@@ -0,0 +1,12 @@
+package com.example.a11st_application;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxofficeList= new ArrayList<Movie>();
+
+}
